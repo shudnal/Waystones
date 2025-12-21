9d19dc4f07f03980ad3e1da46c4151679b3afac4
Suden <20457922+shudnal@users.noreply.github.com>
2025-12-21 19:11:23 +0300

    embedded translations

---
+++ UpdateTranslations.ps1
@@ -0,0 +1,32 @@
+﻿$csvPath = "EmbeddedLocalizations.csv"
+$table = Import-Csv -Path $csvPath -Delimiter ';'
+$headers = $table | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name
+$languages = $headers | Where-Object { $_ -ne 'Token' }
+
+foreach ($lang in $languages) {
+    $json = [ordered]@{}
+    foreach ($row in $table) {
+        $token = $row.Token
+        if ($token) {
+            $translation = $row.$lang
+            $safeValue = $translation -replace "`r", "\n" -replace "`n", "\n"
+            $json[$token] = $safeValue
+        }
+    }
+
+    $outFile = "Translations\$lang.json"
+
+    $content = $json |
+        ConvertTo-Json -Depth 10 |
+        ForEach-Object {
+            $_ -replace ':  ', ': ' `
+               -replace '\\u003c', '<' `
+               -replace '\\u003e', '>' `
+               -replace '\\u0027', "'" `
+               -replace '\\\\n', '\n' `
+               -replace ' :', ' :'
+        }
+
+    $utf8NoBom = New-Object System.Text.UTF8Encoding($false)
+    [System.IO.File]::WriteAllText($outFile, $content, $utf8NoBom)
+}
